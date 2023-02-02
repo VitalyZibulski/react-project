ded3d32f24d4665a4b4331b2d0558f7a6e97309c
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-02-02 20:57:01 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -11,23 +11,25 @@ import Cart from "./pages/Cart";
 import { useState } from "react";
 
 
-const SearchContext = React.createContext('');
+export const SearchContext = React.createContext('');
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
 
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home searchValue={searchValue} />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+      <SearchContext.Provider value={{searchValue, setSearchValue}}>
+        <Header />
+        <div className="content">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </SearchContext.Provider>
     </div>
   );
 }
